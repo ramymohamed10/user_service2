600dc7263b955ca5780ea73f1fd92c8ebad1b9a3
Ramy Mohamed <engrm10@yahoo.com>
2023-09-19 14:40:06 -0400

    Final Commit

--- user_service.py
+++ user_service.py
@@ -1,12 +1,25 @@
 # user_service.py
 
 from flask import Flask, jsonify
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file into the application's environment
+load_dotenv()
 
 app = Flask(__name__)
+
+# Access the FLASK_APP environment variable
+flask_app = os.environ.get("FLASK_APP")
+
+# You can now use the FLASK_APP variable in your Flask application
+print(f"FLASK_APP: {flask_app}")
+
 
 @app.route("/")
 def home():
     return "User Service is live!"
+
 
 @app.route('/user/<id>')
 def user(id):
@@ -17,8 +30,6 @@ def user(id):
     user_info = users.get(id, {})
     return jsonify(user_info)
 
+
 if __name__ == '__main__':
     app.run(port=5000)
-
-
-
